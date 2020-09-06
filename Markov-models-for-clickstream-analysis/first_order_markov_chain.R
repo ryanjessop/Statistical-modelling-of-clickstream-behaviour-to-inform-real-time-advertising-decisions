6c29989a4199bdf39f0a4985330a803025e440f5
Ryan <ryanjessop987@gmail.com>
2020-09-06 19:13:57 +0100

    Ch5 corrections

--- Markov-models-for-clickstream-analysis/first_order_markov_chain.R
+++ Markov-models-for-clickstream-analysis/first_order_markov_chain.R
@@ -37,19 +37,16 @@ plot_first_order_mc <- function(markov_chain){
 }
 
 plot_first_order_mc_v2 <- function(matrix){
-  legendtitle <- list(yref='paper',xref="paper",y=1.03,x=1.11, text="Transition Probability",showarrow=F)
-  
   plot_ly(
     x = colnames(matrix),
     y = colnames(matrix),
     z = matrix, 
     colors = palette(100),
+    colorbar=list(title='Transition \n Probability', titlefont=list(size=20), tickfont=list(size=20)),
     type = "heatmap",
     zauto = FALSE, zmin = 0, zmax = max(matrix, na.rm = TRUE)) %>%
-    layout(xaxis = list(title="To"),
-           yaxis = list(title="From"),
-           title = "First order Markov chain transition matrix",
-           annotations=legendtitle)
+    layout(xaxis = list(title="To", titlefont=list(size=30), tickfont=list(size=20)),
+           yaxis = list(title="From", titlefont=list(size=30), tickfont=list(size=20)))
 }
 
 plot_network_first_order_mc_v2 <- function(markov_chain,
@@ -120,15 +117,16 @@ plot_network_first_order_mc_v2 <- function(markov_chain,
 
 
 plot_network_first_order_mc_v3 <- function(markov_chain,
-                                           lower_bound=0.001,
+                                           lower_bound=0.01,
                                            state_hits=c(127,830,557,9800,4749,862,620,65,6323,998,66,572,634,235),
                                            node_size_factor=0.06,
                                            edge_width_factor=10,
                                            edge_arrow_size=.8,
                                            edge_curved=.6,
-                                           layout_size=1.2,
+                                           layout_size=1.5,
                                            legend_x=-2.0,
-                                           legend_y=-0.5){
+                                           legend_y=-0.5, 
+                                           edges=full_edges){
   
   transitions_df <- as.data.frame(t(markov_chain@transitions$`1`))
   previous_state <- rownames(transitions_df)
@@ -139,13 +137,13 @@ plot_network_first_order_mc_v3 <- function(markov_chain,
                     value='transition_probability') %>% 
     filter(transition_probability > lower_bound) %>% 
     mutate(mapping_values = round(transition_probability, 2))
-  
+  #print(tidy_df)
   color_map <- data_frame(
     greyscale_color = gray.colors(81, start=0, end=0, alpha=seq(0,0.80,0.01)), 
     mapping_values = seq(0,0.80,0.01))
-  
-  edges <- inner_join(tidy_df, color_map, b="mapping_values")
-  
+  # print(color_map)
+  #edges <- left_join(tidy_df, color_map, b="mapping_values")
+  #print(edges)
   nodes <- data_frame(
     previous_state = previous_state,
     hit_counts = state_hits,
@@ -179,8 +177,8 @@ plot_network_first_order_mc_v3 <- function(markov_chain,
          pch=21,
          col="#777777", 
          pt.bg=colrs, 
-         pt.cex=2, 
-         cex=.8, 
+         pt.cex=3.5, 
+         cex=1.4, 
          bty="n", 
          ncol=1)
 }
@@ -193,7 +191,9 @@ first_mc <- fitMarkovChain(cls)
 first_mc_trans <- t(as.matrix(first_mc@transitions[[1]]))
 
 plot_first_order_mc(first_mc)
-plot_first_order_mc_v2(first_mc_trans)
+fig = plot_first_order_mc_v2(first_mc_trans)
 
 plot_network_first_order_mc_v2(first_mc, layout_size = 1.2, node_size_factor=2)
 plot_network_first_order_mc_v3(first_mc, layout_size = 1, node_size_factor=2)
+
+orca(fig, file = "images/first_order_markov_chain_matrix.pdf")
