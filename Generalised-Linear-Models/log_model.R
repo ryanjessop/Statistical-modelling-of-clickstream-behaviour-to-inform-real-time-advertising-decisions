28c0b2f87591a4fd8ae270b16627140ab76024ab
Ryan <ryanjessop987@gmail.com>
2020-09-06 19:10:54 +0100

    Ch4 corrections

--- Generalised-Linear-Models/log_model.R
+++ Generalised-Linear-Models/log_model.R
@@ -25,10 +25,12 @@ binnedplot(fitted(logistic_regression_model),
            nclass = NULL, 
            xlab = "Expected Values", 
            ylab = "Average residual", 
-           main = "Binned residual plot", 
+           main = "", 
            cex.pts = 0.8, 
            col.pts = 1, 
-           col.int = "gray")
+           col.int = "gray", 
+           cex.lab=2.5, 
+           cex.axis=2)
 
 residuals(logistic_regression_model, type = "deviance")
 anova(logistic_regression_model)
@@ -38,8 +40,11 @@ outliers_in_data <- find_outlier_resid_func(train, residuals(logistic_regression
 train_wo_outliers <- remove_outliers_func(train, outliers_in_data)
 
 logistic_predictions <- predict(logistic_regression_model, test, type = 'response')
-hist(logistic_predictions, xlab = 'Predicted probabilities', main = '')
-abline(v = 0.1, col='red')
+logistic_predictions <- logistic_predictions[which(logistic_predictions>0.05)]
+mar.default <- c(5,4,4,2) + 0.1
+par(mar = mar.default + c(0, 2, 0, 0))
+hist(logistic_predictions, xlab = 'Predicted probabilities', main = '', cex.lab=2.5, cex.axis=2)
+abline(v = 0.1, col='red', lwd=3)
 logistic_predictions_bool <- logistic_predictions > 0.1
 table(logistic_predictions_bool, test$click_bool)
 
--- Generalised-Linear-Models/zip_model.R
+++ Generalised-Linear-Models/zip_model.R
@@ -14,7 +14,9 @@ plot(zip_model$residuals, ylab = 'Residuals')
 plot(zip_model$fitted.values,
      resid(zip_model), 
      xlab = 'Fitted values',
-     ylab = 'Residuals')
+     ylab = 'Residuals',
+     cex.lab=2.5, 
+     cex.axis=2)
 
 identify(zip_model$residuals, zip_model$fitted.values)  # 2 huge outliers 
 
@@ -23,8 +25,21 @@ var(zip_model$fitted.values)  # Check for mean = variance
 hist(zip_model$fitted.values)
 
 zip_predictions <- round(predict(zip_model, test, type = 'response'))
-hist(zip_predictions, main = '', xlab = 'Predicted count')
+zip_predictions <- zip_predictions[which(zip_predictions>0)]
+zip_predictions <- zip_predictions[which(zip_predictions<=31)]
+hist(zip_predictions, main = '', xlab = 'Predicted count', cex.lab=2.5, cex.axis=2, breaks=seq(1,31,1))
 table(zip_predictions, test$ClicksDelta)
 
 AIC(zip_model)
 BIC(zip_model)
+
+
+zero_index <- which(test$ImpressionDelta==0)
+zip_predictions <- zip_predictions[which(zip_predictions<=31)]
+hist(zip_predictions, main = '', xlab = 'Predicted count', cex.lab=2.5, cex.axis=2, breaks=seq(1,31,1))
+table(zip_predictions[zero_index], test$ClicksDelta[zero_index])
+
+table(zip_predictions[-zero_index], test$ClicksDelta[-zero_index])
+
+1/(1+exp(-(17.59 - 16.79*4)))
+
